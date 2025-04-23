fc5f216978abd42f8bd9c3b1920049fd18d8cab5
chang180 <chang180@gmail.com>
2025-04-23 21:27:18 +0800

    starts to fetch data from api

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode, useState } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./main.css";
 import App from "./App.js";
@@ -9,6 +9,7 @@ import { NewImageForm } from "./components/NewImageForm";
 import { images as imagesData } from "./data/images";
 import { LikedContext } from "./context/liked-context";
 import { Image } from "./types";
+import { Loader } from "lucide-react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -22,6 +23,7 @@ export function Main() {
   const [images, setImages] = useState<Image[]>(imagesData);
   return (
     <main>
+      <ApiImages />
       <LikedContext value={{ liked, setLiked }}>
         <div className="mt-24 grid gap-8 sm:grid-cols-2">
           <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
@@ -29,7 +31,44 @@ export function Main() {
         </div>
         <ImageList images={images} searchQuery={searchQuery} />
       </LikedContext>
-      <NewImageForm images={images} setImages={setImages}/>
+      <NewImageForm images={images} setImages={setImages} />
     </main>
+  );
+}
+
+export function ApiImages() {
+  const [apiImages, setApiImages] = useState<[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  useEffect(() => {
+    async function getImages() {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://laravel12-sail-xdebug.test/api/images");
+        if (!response.ok) {
+          const errorData = await response.json();
+          setError(errorData.message);
+          throw errorData;
+        }
+        const data = await response.json();
+        setApiImages(data);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        setError("Failed to fetch images");
+      }
+      setIsLoading(false);
+    }
+    getImages();
+  }, [
+    // re-run the effect
+
+  ]);
+
+  return (
+    <div className="bg-white p-6 mt-12 shadow ring ring-black/5">
+      {isLoading && <Loader className="animate-spin stroke-slate-300" />}
+      {error && <p className="text-red-500">{error}</p>}
+      {!error && !isLoading && <pre>{JSON.stringify(apiImages, null, 2)}</pre>}
+    </div>
   );
 }
